20ec09264a46430dd2d253356ec4f8f1ba456418
zhanara <rin94574@gmail.com>
2025-05-20 22:22:53 +0600

    render

--- app.py
+++ app.py
@@ -150,4 +150,4 @@ def handle_send_message(data):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    socketio.run(app, debug=True)
+    socketio.run(app, host='0.0.0.0', port=5000)
