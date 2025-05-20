58860f95689c8cfafd5d10b9e8953122cf92153e
zhanara <rin94574@gmail.com>
2025-05-20 23:20:03 +0600

    2

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 class Config:
     SECRET_KEY = 'your-secret-key'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:1234@localhost:5432/messenger'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://zhanara:Gy0WtVssAiYmv98RIGm77MjYhhko8GzG@dpg-d0mbg163jp1c7386u6g0-a.oregon-postgres.render.com/messenger_3135'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
