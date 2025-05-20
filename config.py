879b40e0a73377c9760b43737b6f9cb34191f650
zhanara <rin94574@gmail.com>
2025-05-20 21:34:19 +0600

    start minimum

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 class Config:
     SECRET_KEY = 'your-secret-key'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:janara2004@localhost:8080/chat_db'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:1234@localhost:5432/messenger'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
